c785024982ded5e6e7c956a76285ddd53da3c632
Jan Szczuryk <jan.szczuryk@gmail.com>
2021-02-05 17:46:44 +0100

    Fixed encoding OEM 852

--- Languages/Polish/wylkomp.bat
+++ Languages/Polish/wylkomp.bat
@@ -1,17 +1,17 @@
 @echo off
-title wylkomp - Zaplanuj wy³¹czenie komputera
+title wylkomp - Zaplanuj wyˆ¥czenie komputera
 :menu
 cls
-echo 1. Wy³¹cz komputer za okreœlony czas.
-echo 2. Anuluj zaplanowane wy³¹czenie komputera.
-echo 3. Wyjœcie.
+echo 1. Wyˆ¥cz komputer za okre˜lony czas.
+echo 2. Anuluj zaplanowane wyˆ¥czenie komputera.
+echo 3. Wyj˜cie.
 choice /c 123 > nul
 if %errorlevel%==1 goto plan
 if %errorlevel%==2 goto abortturnoff
 if %errorlevel%==3 goto quit
 :plan
 cls
-echo Planowanie wy³¹czenia komputera za okreœlony czas.
+echo Planowanie wyˆ¥czenia komputera za okre˜lony czas.
 echo.
 echo Podaj godziny [h]:
 set /p hours="> "
@@ -20,15 +20,15 @@ set /p minutes="> "
 set /a time=%hours%*3600+%minutes%*60
 shutdown -s -t %time%
 echo.
-echo Zaplanowano wy³¹czenie komputera za %hours%h %minutes%min.
+echo Zaplanowano wyˆ¥czenie komputera za %hours%h %minutes%min.
 pause > nul
 goto menu
 :abortturnoff
 cls
 shutdown -a
-echo Anulowanie zaplanowanego wy³¹czenia komputera.
+echo Anulowanie zaplanowanego wyˆ¥czenia komputera.
 echo.
-echo Planowane wy³¹czenie komputera zosta³o anulowane.
+echo Planowane wyˆ¥czenie komputera zostaˆo anulowane.
 pause > nul
 goto menu
 :quit
